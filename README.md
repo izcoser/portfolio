76dc74da4f6ea9af68ed7ea8a03cfbd935025921
Igor <76838137+izcoser@users.noreply.github.com>
2023-05-18 14:19:45 -0300

    Add picture
    And some other options

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+<p align="center">
+  <img src="https://github.com/izcoser/portfolio/blob/main/example.png" />
+</p>
+
 # portfolio
-Short script for keeping track of cryptocurrency portfolios, with NFT support. It uses the Coingecko and OpenSea free endpoints.
+Short script for keeping track of cryptocurrency portfolios, with NFT support. It uses the Coingecko and OpenSea free endpoints. Holdings and display options are stored in a plain text configuration file ```portfolio.txt```.
 
 # Requirements
 Python3.6+ and requests.
@@ -10,6 +14,6 @@ python -m pip install requests
 
 # Run
 ```sh
-# make sure to read and modify portfolio.txt according to your holdings
+# Make sure to read and modify portfolio.txt according to your preferences.
 python portfolio.py
 ```
--- portfolio.py
+++ portfolio.py
@@ -12,9 +12,16 @@ def make_nft_request(collection_slug: str) -> dict:
         print(e)
         return 0
 
-def make_request(currency: str):
+def make_request(portfolio: dict, currency: str, refetch: int, show_btc: bool, show_eth: bool):
     nfts = [nft.replace('NFT_', '') for nft in portfolio if nft.startswith('NFT_')]
     nft_floors = { 'NFT_' + nft: make_nft_request(nft) for nft in nfts }
+
+    if (show_btc or show_eth): # fetch btc or eth values if the user wants to but doesn't have any.
+        if 'bitcoin' not in portfolio:
+            portfolio['bitcoin'] = 0
+        if 'ethereum' not in portfolio:
+            portfolio['ethereum'] = 0
+        
 
     r = requests.get('https://api.coingecko.com/api/v3/simple/price', params={'ids': ','.join([cur for cur in portfolio if not cur.startswith('NFT_')]), 'vs_currencies': currency, 'include_24hr_change': 'true'})
     if r.status_code == 200:
@@ -32,29 +39,45 @@ def make_request(currency: str):
     print(f"{'Coin':<15}{f'Price ({currency})':>10}{'Stack':>20}{f'Holdings ({currency})':>20}")
 
     for key in response:
-        print(f"{key[:14]:<15}{response[key][currency]:>10}{portfolio[key]:>20}{response[key][currency] * portfolio[key]:>20}")
+        print(f"{key[:14]:<15}{response[key][currency]:>10}{portfolio[key]:>20}{round(response[key][currency] * portfolio[key], 2):>20}")
         total += response[key][currency] * portfolio[key]
 
+    btc_total = total / response['bitcoin'][currency]
+    eth_total = total / response['ethereum'][currency]
+
     print(f'\nTotal ({currency}): {round(total, 2)}')
+    if show_btc:
+        print(f'\nTotal (btc): {round(btc_total, 2)}')
+    if show_eth:
+        print(f'\nTotal (eth): {round(eth_total, 2)}')
 
 if __name__ == "__main__":
     with open('portfolio.txt', 'r') as f:
         portfolio = {}
+        options = ('currency', 'refetch', 'show-btc-value', 'show-eth-value')
         currency = 'usd'
         refetch = 180
-        for line in filter(lambda l: not l.startswith(('#', '\n')), f.readlines()):
-            key, value = line.split(':')
-            if key not in ('currency', 'refetch'):
-                value = float(value)
-                portfolio[key] = value
-            elif key == 'currency':
-                currency = value.strip()
-            else:
-                refetch = int(value)
+        show_btc = False
+        show_eth = False
+        lines = filter(lambda l: not l.startswith(('#', '\n')), f.readlines())
+        keys_values = [tuple(line.split(':')) for line in lines]
+        
+        for k, v in filter(lambda tup: tup[0] not in options, keys_values):
+            portfolio[k] = float(v)
+        
+        for k, v in filter(lambda tup: tup[0] in options, keys_values):
+            if k == 'currency':
+                currency = v.strip()
+            elif k == 'refetch':
+                refetch = int(v)
+            elif k == 'show-btc-value' and v.strip().lower() == 'true':
+                show_btc = True
+            elif k == 'show-eth-value' and v.strip().lower() == 'true':
+                show_eth = True
 
     clear = 'cls' if 'Win' in platform.system() else 'clear'
     while True:
-        make_request(currency)
+        make_request(portfolio, currency, refetch, show_btc, show_eth)
         time.sleep(refetch)
         os.system(clear)
 
